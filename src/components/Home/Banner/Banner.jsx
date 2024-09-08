2fdf7a6ab54b73dcadc789bdf8062df3f8a132d9
Mahmud Preyom <preyom5076@example.com>
2024-09-08 22:01:02 +0600

    banner done

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -2,6 +2,8 @@
 import { FaStar } from "react-icons/fa";
 import img1 from "../../../assets/Illustration.png"
 import img2 from "../../../assets/ya.png"
+import img3 from "../../../assets/Group 8431.png"
+import Counter from "../Counter/NCounter";
 
 const Banner = () => {
     return (
@@ -9,12 +11,17 @@ const Banner = () => {
             <div className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="relative">
-                        <img className="w-[660px] h-[745px]" src={img1} alt="" />
-                        <div className="rounded-full w-[288px] h-[70px] bg-slate-50 p-1 flex justify-start absolute bottom-32 -left-10">
+                        <img className="md:w-[660px] lg:w-[660px] w-full md:h-[745px] lg:h-[745px] h-full" src={img1} alt="" />
+                        <div className="rounded-full w-[288px] h-[70px] bg-slate-50 p-1 flex justify-start absolute md:bottom-24 lg:bottom-24 bottom-0 md:-left-10 lg:-left-10 left-0">
                             <img className="w-[60px] h-[60px]" src={img2} alt="" />
-                            <div>
-                            <p className="font-bold">Our Happy Customer</p>
-                            <small className="flex gap-2"><FaStar className="text-yellow-400 mt-1"/> 4.9 <span>(1.989 Reviews)</span></small>
+                            <div className="flex gap-28">
+                                <div className="w-40">
+                                    <p className="font-bold">Our Happy Customer</p>
+                                    <small className="flex gap-2"><FaStar className="text-yellow-400 mt-1" /> 4.9 <span>(1.989 Reviews)</span></small>
+                                </div>
+                                <div className="w-[160px] h-[185px] rounded-3xl bg-slate-50 p-5">
+                                    <img className="mx-auto mt-20" src={img3} alt="" />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -27,6 +34,11 @@ const Banner = () => {
                             healthy and delicious food for you!
                         </p>
                         <button className="btn btn-warning rounded-full text-white">Order Food</button>
+                        <div className="flex gap-5 mt-10">
+                            <p className="font-bold"><Counter className="grid" number={5000} title="+" /><span className="font-normal">Customer</span></p>
+                            <p className="font-bold"><Counter className="grid" number={8000} title="+" /><span className="font-normal">Delivery</span></p>
+                            <p className="font-bold"><Counter className="grid" number={1000} title="+" /><span className="font-normal">Ratings</span></p>
+                        </div>
                     </div>
                 </div>
             </div>
